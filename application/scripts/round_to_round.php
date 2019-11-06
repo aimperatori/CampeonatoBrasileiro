331c267e598c3bcf515c9ca07581684727f3030f
anderson <anderson@anderson-Vostro-5470>
2019-11-05 22:24:59 -0200

    Round-robin tournament

--- application/scripts/round_to_round.php
+++ application/scripts/round_to_round.php
@@ -1,15 +1,41 @@
 <?php
-$teams = array('Equipo 1','Equipo 2','Equipo 3','Equipo 4',"Equipo 5", "Equipo 6");
-$results = array();
 
-foreach($teams as $k){
-	foreach($teams as $j){
-		if($k == $j){ break; }
+/**
+ * @author D.D.M. van Zelst
+ * @copyright 2012
+ */
 
-		$z = array($k,$j);
-		sort($z);
-		if(!in_array($z,$results)){
-			$results[] = $z;
+function scheduler($teams){
+	if (count($teams)%2 != 0){
+		array_push($teams, "bye");
+	}
+	$away = array_splice($teams,(count($teams)/2));
+	$home = $teams;
+	for ($i=0; $i < count($home)+count($away)-1; $i++){
+		for ($j=0; $j<count($home); $j++){
+			$round[$i][$j]['Home'] = $home[$j];
+			$round[$i][$j]['Away'] = $away[$j];
+		}
+		if(count($home)+count($away)-1 > 2){
+			array_unshift($away, array_shift(array_splice($home, 1, 1)));
+			array_push($home, array_pop($away));
 		}
 	}
+	return $round;
 }
+
+
+$members = array("name1","name2","name3","name4");
+
+
+$schedule = scheduler($members);
+
+
+foreach($schedule AS $round => $games){
+	echo "Round: ".($round+1). PHP_EOL;
+	foreach($games AS $play){
+		echo $play["Home"]." - ".$play["Away"]. PHP_EOL;
+	}
+	echo PHP_EOL;
+}
+
