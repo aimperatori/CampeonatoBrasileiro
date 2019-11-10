dd6cec2936128a325c79ee61a8704e038e00136c
aimperatori <aimperatori@ucs.br>
2019-11-10 11:58:35 -0200

    Base da view da listagem dos campeonatos

--- application/controllers/Campeonatos.php
+++ application/controllers/Campeonatos.php
@@ -4,7 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Campeonatos extends CI_Controller {
 	
 	public function index()	{
-		$this->template->show('campeonatos');
+		
+		$data['campeonatos'][] = array(
+					'nome' => 'campeonato brasileiro',
+					'descricao' => 'descrica .........'
+		);
+		
+		$this->template->show('campeonatos', $data);
 	}
 	
 }
--- application/views/campeonatos.php
+++ application/views/campeonatos.php
@@ -1,6 +1,22 @@
-<?php
 
-echo 'lista todos os campeonatos';
-
-?>
-<a href="rodadas">Campeonato Brasileiro</a>
+<div class="container">
+	<div class="row">
+	
+	<?php foreach ($campeonatos as $campeonato) { ?>
+	
+		<div class="col-sm-3">
+			<div class="card">
+				<div class="card-body">
+					<h3 class="card-title"><?php echo $campeonato['nome']; ?></h3>
+					<p class="card-text">
+						<?php echo $campeonato['descricao']; ?>
+					</p>
+					<a href="rodadas" class="btn btn-primary">Ver classificaçao</a>
+				</div>
+			</div>
+		</div>
+		
+	<?php } ?>
+	
+	</div>
+</div>
