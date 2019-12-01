407013f162bac55fe6deda81084d35ca82f725f1
aimperatori <aimperatori@ucs.br>
2019-12-01 15:39:17 -0200

    Base CLI

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,9 +1,12 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+use application\models\Campeonato;
+
 class Home extends CI_Controller {
-	
+
 	public function index()	{
-		$this->template->show('home');		
+		$this->template->show('home');
 	}
 }
